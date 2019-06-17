4d137e7a7f4a2c46acab2caad05e2d5622c9331e
wenghuanjia <1752851362@qq.com>
2019-06-17 18:10:26 +0800

    190617

--- src/service/env.js
+++ src/service/env.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV == 'development') {
     imgBaseUrl = "http://qiniu.gzwmdy.cn/";
     // 线上环境
 } else if (process.env.NODE_ENV == 'production') {
-    host = "http://word1.cn1.utools.club/api";
+    host = "http://link.gzwmdy.cn/api";
     // host = "http://word.gzwmdy.cn/api";
     // host = "http://192.168.52.140/api";
     imgBaseUrl = "http://qiniu.gzwmdy.cn/";
